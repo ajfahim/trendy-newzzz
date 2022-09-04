6d8501a0cc08cc03b7797f8e1ada187e6f49a60c
ajfahim <ajfahim52@gmail.com>
2022-09-04 09:49:17 +0600

    modal added and working

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ const renderCategories = async ()=>{
         ul.classList.add("nav");
         ul.innerHTML= `
         <li class="nav-item">
-            <a class="nav-link text-secondary fw-bold" aria-current="page" href="#">${category.category_name}</a>
+            <a onClick = renderNewsCards('${category.category_id}') class="nav-link text-secondary fw-bold" aria-current="page" href="#">${category.category_name}</a>
         </li>
         `;
         categoryItems.appendChild(ul)
@@ -31,8 +31,8 @@ const renderCategories = async ()=>{
 renderCategories();
 
 
-const getNewsByCaterory = async () => {
-    const url = 'https://openapi.programming-hero.com/api/news/category/01';
+const getNewsByCaterory = async id => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
     try {
         const res = await fetch(url);
         const data = await res.json();
@@ -45,16 +45,51 @@ const getNewsByCaterory = async () => {
 
 }
 
-const renderNewsCards = async ()=>{
-    const cards = await getNewsByCaterory();
+const getCategoryNameById = async id =>{
+    const categories = await getCategories()
+    const url = 'https://openapi.programming-hero.com/api/news/categories';
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log( "I have data ", data.data.news_category)
+        await data.data.news_category.forEach( async category =>{
+            if(category.category_id === id){
+                 return await category.category_name
+            }
+        })
+       
+    }
+    catch (err) {
+        console.log(err)
+    }
+
+  
+}
+
+const setItemCountAndCategoryName = (number, name)=>{
+    const resultCount = document.getElementById("result-count");
+    const categoryName = document.getElementById("category-name");
+    resultCount.innerText = number;
+    categoryName.innerText = name; 
+    // console.log(number, name)
+}
+// setItemCount();
+
+
+
+const renderNewsCards = async id=>{
+    const cards = await getNewsByCaterory(id);
+    const name = await getCategoryNameById(id)
+   await setItemCountAndCategoryName(cards.length, name);
+
     const cardsElement = document.getElementById("cards");
-    
+    cardsElement.innerHTML ="";
     cards.forEach( card =>{
         const div = document.createElement("div");
         div.classList.add("card", "mb-3");
         div.innerHTML =`
-        <div class="row g-1">
-                    <div class="col-md-4">
+        <div class="row">
+                    <div class="col-md-4 text-center">
                         <img src="${card.thumbnail_url}" class="img-fluid rounded-start p-2" alt="...">
                     </div>
                     <div class="col-md-8">
@@ -94,7 +129,7 @@ const renderNewsCards = async ()=>{
                                     </div>
                                     <div class="col">
                                         <div class="  text-center">
-                                            <a href="#"><i
+                                            <a onClick = renderModal('${card._id}') href="#" data-bs-toggle="modal" data-bs-target="#modal"><i
                                                     class="fa-solid fa-arrow-right fw-bold text-primary fs-3"></i></a>
 
                                         </div>
@@ -109,6 +144,43 @@ const renderNewsCards = async ()=>{
         `;
         cardsElement.appendChild(div)
     })
+    
+}
+renderNewsCards('08');
+
+
+const getNewsDetailsById = async (id) => {
+    const url = `https://openapi.programming-hero.com/api/news/${id}`;
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log("ndslfjasld;jfslkj",data.data[0])
+        return await data.data[0];
+    }
+    catch (err) {
+        console.log(err)
+    }
 
 }
-renderNewsCards();
+// getNewsDetailsById();
+
+const renderModal = async (id)=>{
+    const modalDetalis = await getNewsDetailsById(id);
+    const modalElement = document.getElementById("modal-elemant");
+    const div = document.createElement("div");
+    modalElement.innerHTML="";
+    div.innerHTML = `
+        <h1>${modalDetalis.title}</h1>
+        <p class="text-muted">Posted on <span class="fw-bold">${modalDetalis.author.published_date ? modalDetalis.author.published_date.slice(0,11) : "Date Not Found"}</span></p>
+        <p class="text-muted">Posted by <span class="fw-bold">${modalDetalis.author.name ? modalDetalis.author.name : "Author Name Not Found"}</span></p>
+        <p class="text-muted">Total View: <span class="fw-bold"> ${modalDetalis.total_view ? modalDetalis.total_view : "Data Not Found"}</span></p>
+        <div class="my-3">
+            <img class="img-fluid" src="${modalDetalis.image_url}" alt="">
+        </div>
+        <div>
+            <p>${modalDetalis.details}</p>
+        </div>
+    `;
+    modalElement.appendChild(div);
+    
+}
